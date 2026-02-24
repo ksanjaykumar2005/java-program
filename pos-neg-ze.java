574570bfd0cd648dda3a4c174a172d14cdd33644
ksanjaykumar2005 <sanjaykannappan2005@gmail.com>
2026-02-24 13:07:32 +0530

    pos-neg-zed.java file added

---
+++ pos-neg-ze.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        if(num>0){
+            System.out.println("Positive");
+        }else if(num<0){
+            System.out.println("Negative");
+        }else{
+            System.out.println("Zero");
+        }
+    }
+}
